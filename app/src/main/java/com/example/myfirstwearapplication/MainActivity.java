129b25b84ee0b9c4cdfc03c5468949b4d0651a17
Rohan Rajpal <rohan17089@iiitd.ac.in>
2020-02-23 02:45:13 +0530

    complete speech code

--- app/src/main/java/com/example/myfirstwearapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstwearapplication/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.myfirstwearapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.wear.activity.ConfirmationActivity;
+
+import java.util.List;
 
 public class MainActivity extends WearableActivity {
 
@@ -11,11 +20,48 @@ public class MainActivity extends WearableActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //means app will look into the resources folder, then the layouts folder and finally the activity main
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        Button b1 = findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText("WARDI 2020");
+
+                displaySpeechRecognizer();
+            }
+        });
+
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            mTextView.setText(spokenText);
+            // Do something with spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
