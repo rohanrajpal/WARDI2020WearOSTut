e697483f31a4aa7f7c9e5f9811d21f5c971f5c0d
Rohan Rajpal <rohan17089@iiitd.ac.in>
2020-02-23 02:48:50 +0530

    add button to change textview

--- app/src/main/java/com/example/myfirstwearapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstwearapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myfirstwearapplication;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
@@ -11,9 +13,18 @@ public class MainActivity extends WearableActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //means app will look into the resources folder, then the layouts folder and finally the activity main
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
+
+        Button b1 = findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextView.setText("WARDI 2020");
+            }
+        });
 
         // Enables Always-on
         setAmbientEnabled();
